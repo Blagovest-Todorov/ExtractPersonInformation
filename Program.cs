1581a2d4f1754b41dfe2b8203d86fd3bd1b991e5
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-03-17 19:29:34 +0200

    add program.cs

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+namespace ExtractPersonInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nLines = int.Parse(Console.ReadLine());
+
+            
+            
+
+            for (int i = 0; i < nLines; i++)
+            {
+                string text = Console.ReadLine();
+
+                string sbName = string.Empty;
+                string sbAge = string.Empty;
+
+                if ( text.Contains('@') && text.Contains('|') && text.Contains('#') &&  text.Contains('*') )
+                {
+                    int idxOfAtSymbol = text.IndexOf('@');  //start idx +1  ->  Name
+                    int idxOfPipeSymbol = text.IndexOf('|'); //endIdx -1 / Name
+                    int idxOfHashTagSymbol = text.IndexOf('#'); //startIdx +1  of Age
+                    int idxOfAstericSymbol = text.IndexOf('*'); //endIdx -1 of Age
+
+                    for (int j = idxOfAtSymbol + 1; j < idxOfPipeSymbol; j++)
+                    {
+                        sbName += text[j];
+                    }
+
+                    for (int k = idxOfHashTagSymbol + 1 ; k < idxOfAstericSymbol; k++)
+                    {
+                        sbAge += text[k];
+                    }
+                }
+
+                Console.WriteLine($"{sbName} is {sbAge} years old.");
+                
+            }
+        }
+    }
+}
